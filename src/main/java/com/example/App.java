4e7a513f25729da2d5cb050698fc97eafd10873c
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-08-30 09:35:48 +0200

    Create display menu

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ATM MACHINE");
+        System.out.println("***********************");
+        System.out.println("1. Show Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+        System.out.println("***********************");
+
+        System.out.print("Enter your choice (1-4): ");
+        choice = scanner.nextInt();
+
+        scanner.close();
     }
 }
