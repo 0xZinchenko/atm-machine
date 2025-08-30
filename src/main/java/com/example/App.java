576eb7b880deee02d8b80a4432a80084473cf2d1
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-08-30 09:53:47 +0200

    Create user input

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,16 +10,27 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("ATM MACHINE");
-        System.out.println("***********************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("***********************");
+        while (isRunning) {
+            System.out.println("***********************");
+            System.out.println("ATM MACHINE");
+            System.out.println("***********************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***********************");
 
-        System.out.print("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> System.out.println("SHOW BALANCE");
+                case 2 -> System.out.println("DEPOSIT");
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
 
         scanner.close();
     }
