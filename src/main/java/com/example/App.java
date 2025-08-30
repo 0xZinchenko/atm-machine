33cbe9cd177ed00ff51bf4ead738306dd478babf
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-08-30 10:10:22 +0200

    Create methods showBalance and deposit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,12 +3,12 @@ package com.example;
 import java.util.Scanner;
 
 public class App {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        double balance;
+        double balance = 0;
         boolean isRunning = true;
         int choice;
-
-        Scanner scanner = new Scanner(System.in);
 
         while (isRunning) {
             System.out.println("***********************");
@@ -24,8 +24,8 @@ public class App {
             choice = scanner.nextInt();
 
             switch (choice) {
-                case 1 -> System.out.println("SHOW BALANCE");
-                case 2 -> System.out.println("DEPOSIT");
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
                 case 3 -> System.out.println("WITHDRAW");
                 case 4 -> isRunning = false;
                 default -> System.out.println("INVALID CHOICE");
@@ -33,5 +33,22 @@ public class App {
         }
 
         scanner.close();
+    }
+
+    static void showBalance(double balance) {
+        System.out.println("***********************");
+        System.out.printf("$%.2f\n", balance);
+    }
+
+    static double deposit() {
+        double amount;
+        System.out.println("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+        if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+        } else {
+            return amount;
+        }
     }
 }
